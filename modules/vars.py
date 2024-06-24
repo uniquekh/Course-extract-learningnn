0f9c782a0b1f50b0239fe3f0911b91a8bc2016d0
Unique <mujeem000@gmail.com>
2024-06-24 19:07:48 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "24271143"
-api_hash = "27be842cb506de9b5520146dfd0ba299"
-bot_token = "7388410678:AAHtg6IVDpCCiD3QydriEVrsuKLYbRJ-Mb0"
+api_id = "21179966"
+api_hash = "d97919fb0a3c725e8bb2a25bbb37d57c"
+bot_token = "7396110465:AAHlEweczT7b7B1w8MJW2eBHAWJRSTloFeo"
